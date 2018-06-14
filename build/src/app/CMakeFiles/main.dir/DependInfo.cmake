a5e359bc5c8b756ace7e2bc7dae8d6e3ac771c5e
L <731278512@qq.com>
2018-06-14 22:54:09 +0800

    create project

---
+++ build/src/app/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/l/Workspace/webserver/src/app/main.cpp" "/home/l/Workspace/webserver/build/src/app/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/l/Workspace/webserver/build/src/module/CMakeFiles/hello.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/module/hello.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author:L
+	> Mail:731278512@qq.com
+	> Created Time: 2018年06月13日 星期三 00时05分02秒
+ ************************************************************************/
+
+#include<iostream>
+#include "../../include/hello.h"
+using namespace std;
+
+void hello(void)
+{
+	cout<<"hello cmake"<<endl;
+}
+
